865d4df440b2555fbb787d5d23bb436b3330633c
kahturin <karinaalveslima@icloud.com>
2025-04-03 21:08:12 -0300

    reestruturação do projeto e criação de novas classes + métodos

---
+++ src/main/java/br/com/enviodeemails/Pessoa.java
@@ -0,0 +1,19 @@
+package br.com.enviodeemails;
+
+public class Pessoa {
+    private String nome;
+    private String email;
+
+    public Pessoa(String nome, String email){
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+}
---
+++ src/main/java/br/com/enviodeemails/config/Config.java
@@ -0,0 +1,23 @@
+package br.com.enviodeemails.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+    private static Properties properties;
+
+    static {
+        try {
+            properties = new Properties();
+            FileInputStream file = new FileInputStream("src/test/resources/config.properties");
+            properties.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar config.properties: " + e.getMessage());
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
